f9b1ceb42f578c32985b72982a3e948edb4f23de
Judah Goff <35714383+bgoff1@users.noreply.github.com>
2018-06-20 12:32:55 -0500

    Added dad jokes

--- bot.js
+++ bot.js
@@ -2,6 +2,7 @@
 var Discord = require('discord.io');
 var logger = require('winston');
 var auth = require('./auth.json');
+var dad = require('./dadjokes');
 // Configure logger settings
 logger.remove(logger.transports.Console);
 logger.add(logger.transports.Console, {
@@ -18,9 +19,20 @@ bot.on('ready', function (evt) {
     logger.info('Logged in as: ');
     logger.info(bot.username + ' - (' + bot.id + ')');
 });
+var laughCount = 0;
 bot.on('message', function (user, userID, channelID, message, evt) {
+    if (message.substring(0, 4) == "haha") {
+        ++laughCount;
+        if (laughCount >= 3 || message.startsWith("hahahahahahahahahaha")) {
+            bot.sendMessage({
+                to: channelID,
+                message: `Stop laughing! I am a bot!`
+            })
+        }
+    }
     if (channelID == '459025629048864768')
     {
+        if (laughCount != 0) laughCount = 0;
         if (message.substring(0, 3) == "I'm" || message.substring(0, 3) == "Im " || message.substring(0, 3) == "im ")
         {
             let cmd = message.substring(3);
@@ -39,6 +51,19 @@ bot.on('message', function (user, userID, channelID, message, evt) {
                         message: 'Pong!'
                     })
                     break;
+                case 'joke':
+                    let joke = dad.dadjokes[Math.floor(Math.random() * dad.dadjokes.length)];
+                    bot.sendMessage({
+                        to: channelID,
+                        message: `${joke}`
+                    })
+                    break;
+                case 'meme':
+                    let name = dad.names[Math.floor(Math.random() * dad.names.length)];
+                    bot.sendMessage({
+                        to: channelID,
+                        message: `You got it ${name}!`
+                    });
             }
         }
     }
